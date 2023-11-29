c46f4e0c6873fdd5aac586c8160864b173a4650d
Lucumerry <145458440+Arlausea@users.noreply.github.com>
2023-11-29 14:02:35 +0100

    Q1 v2

---
+++ enseash_Q2.c
@@ -0,0 +1,26 @@
+#include <unistd.h> //For EXIT_SUCCESS/FAILURE
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> //For strlen
+#include <fcntl.h> //For open/creat
+
+
+void main(int argc,char** argv){
+
+    //Informational Messages
+
+    char welcomeMessage[] = "Bienvenue dans le Shell ENSEA \nPour quitter, tapez 'exit'\n";
+    char waitingPrompt[] = "enseash %\n";
+
+
+    //Variables
+
+    int terminal = STDOUT_FILENO; //Send arguments to terminal
+
+
+    write(terminal,welcomeMessage,strlen(welcomeMessage));
+    write(terminal,waitingPrompt,strlen(waitingPrompt));
+
+    exit(EXIT_SUCCESS);
+
+}
