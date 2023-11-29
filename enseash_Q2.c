7ee76d331e60d625e12451cb3b71725a4d21dfae
Lucumerry <145458440+Arlausea@users.noreply.github.com>
2023-11-29 14:42:51 +0100

    Q2 v0.1

--- enseash_Q2.c
+++ enseash_Q2.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h> //For strlen
 #include <fcntl.h> //For open/creat
+#include <sys/wait.h>
 
 
 void main(int argc,char** argv){
@@ -10,18 +11,43 @@ void main(int argc,char** argv){
     //Informational Messages
 
     char welcomeMessage[] = "Bienvenue dans le Shell ENSEA \nPour quitter, tapez 'exit'\n";
-    char waitingPrompt[] = "enseash %\n";
+    char waitingPrompt[] = "enseash % ";
 
 
     //Variables
 
     //Files Descriptors
     int terminal = STDOUT_FILENO; //Send arguments to terminal
+    int fd_input = STDIN_FILENO; //Get arguments
 
+    //For fork
+    int pid;
+    int intputSize;
+    int status;
+
+    //Input variable
+    char input[] = {0};
+    char inputArgs[] = {0};
 
     //Main code
     write(terminal,welcomeMessage,strlen(welcomeMessage));
     write(terminal,waitingPrompt,strlen(waitingPrompt));
+
+    pid = fork();
+    if (pid == 0){ // Child code
+        
+        intputSize=read(fd_input,input,sizeof(input));
+        while (strcmp(input,"\n")!=0){
+            read(fd_input,input,sizeof(input));
+        }
+        execvp(input,NULL);
+        exit(EXIT_FAILURE);
+    }
+    else{
+        wait(&status);
+        write(terminal,waitingPrompt,strlen(waitingPrompt));
+    }
+
 
     exit(EXIT_SUCCESS);
 
